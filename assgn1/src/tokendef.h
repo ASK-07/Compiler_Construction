f5b3f29cd43ec0f4f1d58bc3947dfea19a95bf05
Jacob Pendergraff <ptd24@txstate.edu>
2024-09-21 14:56:50 -0500

    Scanner.l now correctly identifies all tokens and Tokendef.h has been updated to match.

--- assgn1/src/tokendef.h
+++ assgn1/src/tokendef.h
@@ -3,39 +3,49 @@
 #define INTCONST   252
 #define CHARCONST  253
 #define STRCONST   254
+#define ERROR      255
 
 /* keywords */
-#define KWD_IF     255
-#define KWD_ELSE   256
-#define KWD_WHILE  257
-#define KWD_INT    258
-#define KWD_STRING 259
-#define KWD_CHAR   260
-#define KWD_RETURN 261
-#define KWD_VOID   262
+#define KWD_IF     256
+#define KWD_ELSE   257
+#define KWD_WHILE  258
+#define KWD_INT    259
+#define KWD_STRING 260
+#define KWD_CHAR   261
+#define KWD_RETURN 262
+#define KWD_VOID   263
 
 /* operators */
-#define OPER_ADD    263
-#define OPER_SUB    264
-#define OPER_MUL    265
-#define OPER_DIV    266
-#define OPER_LT     267
-#define OPER_GT     268
-#define OPER_GTE    269
-#define OPER_LTE    270
-#define OPER_EQ     271
-#define OPER_NEQ    272
-#define OPER_ASGN    273
+#define OPER_ADD    264
+#define OPER_SUB    265
+#define OPER_MUL    266
+#define OPER_DIV    267
+#define OPER_MOD    268
+#define OPER_LT     269
+#define OPER_GT     270
+#define OPER_GTE    271
+#define OPER_LTE    272
+#define OPER_EQ     273
+#define OPER_NEQ    274
+#define OPER_ASGN   275
+#define OPER_AT     276
+#define OPER_INC    277
+#define OPER_DEC    278
+#define OPER_AND    279
+#define OPER_OR     280
+#define OPER_NOT    281
 
 /* brackets & parens */
-#define LSQ_BRKT    274
-#define RSQ_BRKT    275
-#define LCRLY_BRKT  276
-#define RCRLY_BRKT  277
-#define LPAREN     278
-#define RPAREN     279
+#define LSQ_BRKT    282
+#define RSQ_BRKT    283
+#define LCRLY_BRKT  284
+#define RCRLY_BRKT  285
+#define LPAREN      286
+#define RPAREN      287
 
 /* punctuation */
-#define COMMA      280
-#define SEMICLN    281
+#define COMMA       288
+#define SEMICLN     289
+
+#define ILLEGAL_TOK 290
 
